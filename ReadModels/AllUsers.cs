e7349a8f2134e4c90ba38923b2600072722415f0
andy <andy@andydote.co.uk>
2016-03-21 21:35:39 +0000

    readmodel: add simple users projection

---
+++ ReadModels/AllUsers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Events;
+using Ledger;
+
+namespace ReadModels
+{
+	public class AllUsers
+	{
+		public Dictionary<Guid, string> Users { get; }
+		 
+		private readonly Dictionary<Type, Action<DomainEvent<Guid>>> _projections;
+
+		public AllUsers()
+		{
+			_projections = new Dictionary<Type, Action<DomainEvent<Guid>>>();
+
+			Users = new Dictionary<Guid, string>();
+
+			Register<UserCreatedEvent>(e => Users[e.AggregateID] = e.Name);
+			Register<UserNameChangedEvent>(e => Users[e.AggregateID] = e.NewName);
+		}
+
+		private void Register<TEvent>(Action<TEvent> projection) where TEvent : DomainEvent<Guid>
+		{
+			_projections[typeof(TEvent)] = e => projection((TEvent)e);
+		}
+
+		public void Project(DomainEvent<Guid> e)
+		{
+			Action<DomainEvent<Guid>> projection;
+
+			if (_projections.TryGetValue(e.GetType(), out projection))
+				projection(e);
+		}
+	}
+}
